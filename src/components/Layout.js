73c2a809c26675cd228212258f7b23659e0f5331
Beimar Medina <bmedina@vitas.com>
2017-11-11 15:09:48 -0500

    comments

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -74,36 +74,31 @@ class Layout extends Component {
           {/* <a href="#" data-activates="slide-out" className="button-collapse show-on-large"><i className="material-icons">menu</i></a> */}
 
           <div id="HOME" className="header-bg" style={homeStyle}>
-          <div style={{color: "#555", position: "absolute", textAlign: "center", top: "300px", left: "0px", verticalAlign: "middle", width: "100%", zIndex: 8, fontFamily: "fantasy", fontSize: "77px", fontWeight: "800" }}>
-            <h2>
-              HI, I&#8217;M <br/>
-              <span style={{color: "#ff0000"}}>
-                Beimar Medina
-              </span>
-            </h2>
-          </div>
-            
+            <div style={{color: "#555", position: "absolute", textAlign: "center", top: "300px", left: "0px", verticalAlign: "middle", width: "100%", zIndex: 8, fontFamily: "fantasy", fontSize: "77px", fontWeight: "800" }}>
+              <h2>
+                HI, I&#8217;M <br/>
+                <span style={{color: "#ff0000"}}>
+                  Beimar Medina
+                </span>
+              </h2>
+            </div>            
           </div>                    
 
-          <article style={divStyle}>
-          <h1 id="ABOUT">2. About</h1>
-          <p>Currently works well on <b>all modern browsers</b> such as recent versions of <b>Chrome, Safari, Opera, Firefox and IE10+</b>. I have also tested this on current mobile operating systems such as <b>iOS6, iOS7, Jellybean and BlackBerry's OS 10 Software</b>.</p>
-          </article>
+          <div id="ABOUT">
+            <div style={{fontFamily: "fantasy", fontSize: "40px", fontWeight: "800", marginLeft: "300px", marginTop: "36px", height: "680px"}}>ABOUT ME<span style={{color: "#ff0000"}}> | </span>Professional Software Developer</div>
+          </div>   
 
-          <article style={divStyle}>
-          <h1 id="SERVICES">3. Services</h1>
-          <p>The current version of the code is maintained on <b>GitHub</b>, this could be an old version!</p>        
-          </article>
+          <div id="SERVICES">
+            <div style={{fontFamily: "fantasy", fontSize: "40px", fontWeight: "800", marginLeft: "300px", marginTop: "20px", height: "680px"}}>MY SERVICES<span style={{color: "#ff0000"}}> | </span>WHAT I CAN DO</div>
+          </div>
 
-          <article style={divStyle}>
-            <h1 id="PORTFOLIO">4. Portfolio</h1>
-            <p>This script was written by Matt Litherland @mattsince87 and is intended for public use. please <b>do not</b> sell this script as an individual product. It may be included in your projects that are sold as an entire website or application.</p>            
-          </article>
+          <div id="PORTFOLIO">
+            <div style={{fontFamily: "fantasy", fontSize: "40px", fontWeight: "800", marginLeft: "300px", marginTop: "20px", height: "680px"}}>PORTFOLIO</div>
+          </div>                       
 
-          <article style={divStyle}>
-            <h1 id="CONTACT">5. Contact</h1>
-            <p>This script was written by Matt Litherland @mattsince87 and is intended for public use. please <b>do not</b> sell this script as an individual product. It may be included in your projects that are sold as an entire website or application.</p>            
-          </article> 
+          <div id="CONTACT">
+            <div style={{fontFamily: "fantasy", fontSize: "40px", fontWeight: "800", marginLeft: "300px", marginTop: "20px", height: "680px"}}>SAY HELLO TO ME</div>
+          </div>          
       </div>
     );
   }
